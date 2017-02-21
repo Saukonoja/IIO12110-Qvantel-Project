e895090f150f5e4bf1c2f078f3cb1ce2c4e48447
root <root@ip-172-31-19-110.ec2.internal>
2017-02-21 16:53:47 +0000

    add menu item translation

--- themes/cassietheme/templates/user-login-block.tpl.php
+++ themes/cassietheme/templates/user-login-block.tpl.php
@@ -1,6 +1,13 @@
+<?php
+
+	include_once drupal_get_path('module', 'cassie').'/cassie.module';
+	$login = set_text("Kirjaudu sisään", "войти");
+	$register = set_text("Rekisteröinti", "регистрация");
+	$adminLogin = set_text("Järjestelmänvalvojan kirjautuminen", "Войти Администратор");
+?>
 <div id="user-login-block-container">
 
   <div class="links">
-    <a href="/login/">Login</a><br> <a href="/registration">Register</a><br> <a href="/user/login">Admin login</a>
+    <a href="/login/"><?php echo $login ?></a><br> <a href="/registration"><?php echo $register ?></a><br> <a href="/user/login"><?php echo $adminLogin ?></a>
   </div>
 </div>
